f22ac960bd369034ce360ea5848ce9fbe0d04a9c
Md Asaduzaman <asadasad9282@gmail.com>
2024-03-04 17:55:26 +0600

    dashboard design

--- src/app/(dashboard)/dashbord/page.jsx
+++ src/app/(dashboard)/dashbord/page.jsx
@@ -5,10 +5,14 @@ import Image from 'next/image';
 
 const Dashboard = () => {
   return (
-    <div className="mt-[50px] flex flex-col lg:flex-row items-center gap-3 xl:gap-6">
+    <div className="mt-[50px] flex flex-col xl:flex-row items-center gap-3 xl:gap-6">
 
-      <div className="w-[350px] md:w-[650px] 2xl:w-[714px]">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-4 gap-y-8 rounded-2xl bg-[#fff]  pl-[27px] pr-[10px] py-20 bg-opacity-[9%]  text-[#E1E1E1]">
+      <div className="relative w-[350px] md:w-[650px] 2xl:w-[714px]">
+        <div className='shape absolute -top-[29.5px] border-t-[30px] rounded-tl-[40px] w-[33%]  border-[#fff] border-opacity-[14%]  border-r-transparent border-l-transparent border-b-transparent '>
+
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-4 gap-y-8 rounded-2xl rounded-tl-none bg-[#fff]  pl-[27px] pr-[10px] py-20 bg-opacity-[14%]  text-[#E1E1E1]">
 
           <div className="border-[1px] flex flex-col justify-center rounded-tr-[130px] p-3 xl:p-5 rounded-2xl  w-[300px] 2xl:w-[330px] h-[150px] xl:h-[170px] border-[#B4B4B4]">
             <p className="text-lg font-semibold">MUSD WALLET</p>
@@ -34,14 +38,13 @@ const Dashboard = () => {
         </div>
       </div>
 
+      <div className="relative w-[350px] xl:w-[350px] mt-7 lg:mt-0 2xl:w-[359px]">
+        <div className='shape2 absolute -top-[29.5px] right-0 ms-auto border-t-[30px] rounded-tr-[40px] w-[33%]  border-[#fff] border-opacity-[14%]  border-r-transparent border-l-transparent border-b-transparent '>
 
-      <div className="w-[350px] lg:w-[320px]  2xl:w-[359px]">
-        <div className=" bg-[#fff] rounded-2xl px-3 xl:pl-[27px] xl:pr-[10px] py-20 bg-opacity-[9%]  text-[#E1E1E1]">
-
+        </div>
+        <div className=" bg-[#fff] rounded-2xl rounded-tr-none px-3 xl:pl-[27px] xl:pr-[10px] py-10 bg-opacity-[14%]  text-[#E1E1E1]">
           <h2 className="text-[18px] font-bold pb-10 ">Task Summary</h2>
-
           <div className='flex flex-col gap-3 xl:gap-6'>
-
             <div className="border-[1px] border-white p-2 pr-5 place-items-center rounded-[40px] flex justify-between  gap-0">
               <div className='flex place-items-center gap-[20px]'>
                 <div className='bg-[#FFFFFF] p-3 rounded-full '>
@@ -92,4 +95,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,13 @@ module.exports = {
     extend: {
       screens: {
         sm: "640px",
+
         md: "768px",
+
         lg: "1024px",
+
         xl: "1280px",
+
         "2xl": "1440px",
       },
       backgroundImage: {
