d339457e012c783164afbfe8a03ce14ab7d54805
Md Asaduzaman <asadasad9282@gmail.com>
2024-03-04 12:05:11 +0600

    changes

--- src/app/(dashboard)/dashbord/page.jsx
+++ src/app/(dashboard)/dashbord/page.jsx
@@ -3,6 +3,18 @@ const Dashboard = () => {
   return (
     <div>
       <h2>Dashboard</h2>
+
+
+      <div className="bg-[#fff] bg-opacity-[9%]  text-[#E1E1E1]">
+
+
+        <div className="border-[1px] rounded-tr-[130px] p-5 rounded-2xl w-[330px] h-[170px] border-gray-400">
+          <p className="text-lg font-semibold">MUSD WALLET</p>
+          <h2 className="font-bold mt-5 text-base">$28.40</h2>
+          <p className="font-medium mt-[28px]">TOTAL PORTFOLIO:  $28.4</p>
+        </div>
+
+      </div>
     </div>
   );
 };
