10b8219a6ff5800fd949e6ae2cdb40169ec95532
Md Asaduzaman <asadasad9282@gmail.com>
2024-03-05 16:36:17 +0600

    changes

--- src/components/pages/dashbord/AvailableTask.jsx
+++ src/components/pages/dashbord/AvailableTask.jsx
@@ -4,15 +4,13 @@ import React from 'react';
 const AvailableTask = () => {
   return (
     <div className='mb-10 max-w-full mt-4 px-7 py-9  bg-[#FFFFFF] text-[#CCCCCC] bg-opacity-[14%]'>
-      <Title title={'Available Task '}></Title>
-
+      <div>
+        <Title className={''} title={'Available Task '}></Title>
+      </div>
 
       <div className='lg:max-w-[730px] xl:max-w-[980px] '>
-
-        <div className='  text-white overflow-x-auto mt-5'>
-
-          <table class=' min-w-[1068px]  border-[#fff]  rounded-[15px] border-2 '>
-
+        <div className='rounded-2xl  text-white overflow-x-auto mt-5'>
+          <table class=' min-w-[1068px] rounded-2xl  border-[#fff] border-[2px]'>
             <thead className='border-b rounded-[15px]'>
               <tr className='divide-x'>
                 <th class='px-6 py-3 text-left text-xs leading-4 font-medium text-[#CCCCCC] uppercase tracking-wider'>
@@ -45,36 +43,36 @@ const AvailableTask = () => {
             <tbody class='text-white   divide-y  divide-[#BDBDBD]'>
               {/* <!-- Table rows go here --> */}
               <tr className='divide-x '>
-                <td class='pr-6 py-4  '>
+                <td class='pr-6 py-4 whitespace-no-wrap'>
                   <div class='ml-4'>
                     <div class='text-sm leading-5 font-medium '>
                       Take 2 or 3 pictures .... day and copy the                    </div>
                   </div>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <div class='text-sm leading-5 '> {"I'm very happy to be with t......MindChain.Join our twitter:"} </div>
                 </td>
-                <td class='px-6 py-4  '>
+                <td class='px-2 py-1  '>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     https:www....com/
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     https:www....com/
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     2
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     21.02.2024
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <button>Button</button>
                 </td>
               </tr>
@@ -85,30 +83,30 @@ const AvailableTask = () => {
                       Take 2 or 3 pictures .... day and copy the                    </div>
                   </div>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <div class='text-sm leading-5 '> {"I'm very happy to be with t......MindChain.Join our twitter:"} </div>
                 </td>
-                <td class='px-6 py-4  '>
+                <td class='px-2 py-1  '>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     https:www....com/
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     https:www....com/
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     2
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     21.02.2024
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <button>Button</button>
                 </td>
               </tr>
@@ -119,30 +117,30 @@ const AvailableTask = () => {
                       Take 2 or 3 pictures .... day and copy the                    </div>
                   </div>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <div class='text-sm leading-5 '> {"I'm very happy to be with t......MindChain.Join our twitter:"} </div>
                 </td>
-                <td class='px-6 py-4  '>
+                <td class='px-2 py-1  '>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     https:www....com/
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     https:www....com/
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     2
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     21.02.2024
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <button>Button</button>
                 </td>
               </tr>
@@ -153,30 +151,30 @@ const AvailableTask = () => {
                       Take 2 or 3 pictures .... day and copy the                    </div>
                   </div>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <div class='text-sm leading-5 '> {"I'm very happy to be with t......MindChain.Join our twitter:"} </div>
                 </td>
-                <td class='px-6 py-4  '>
+                <td class='px-2 py-1  '>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     https:www....com/
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     https:www....com/
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     2
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     21.02.2024
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <button>Button</button>
                 </td>
               </tr>
@@ -187,30 +185,30 @@ const AvailableTask = () => {
                       Take 2 or 3 pictures .... day and copy the                    </div>
                   </div>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <div class='text-sm leading-5 '> {"I'm very happy to be with t......MindChain.Join our twitter:"} </div>
                 </td>
-                <td class='px-6 py-4  '>
+                <td class='px-2 py-1  '>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     https:www....com/
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     https:www....com/
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     2
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <span class='px-2 inline-flex text-xs leading-5 font-semibold  '>
                     21.02.2024
                   </span>
                 </td>
-                <td class='px-6 py-4 whitespace-no-wrap'>
+                <td class='px-2 py-1 whitespace-no-wrap'>
                   <button>Button</button>
                 </td>
               </tr>
--- src/components/ui/title.jsx
+++ src/components/ui/title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = ({ title, className }) => {
   return (
-    <div className={`text-2xl ${className}`}>
+    <div className={`text-2xl font-semibold ${className}`}>
       {title}
     </div>
   );
